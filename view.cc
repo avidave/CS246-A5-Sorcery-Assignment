1307ceb4fdd1c052ad43dea3b6a370c2a6063ddb
amirzaei <amirzaei@uwaterloo.ca>
2023-12-03 13:30:20 -0500

    Fixed Segfault

--- view.cc
+++ view.cc
@@ -56,9 +56,11 @@ void View::board() {
 
     card_template_t p1Ritual;
     card_template_t p2Ritual;
-    if (ritual_1->getName() != "unknown")  p1Ritual = ritual_1->display();
+    if (!ritual_1) p1Ritual = CARD_TEMPLATE_BORDER;
+    else if (ritual_1->getName() != "unknown")  p1Ritual = ritual_1->display();
     else p1Ritual = CARD_TEMPLATE_BORDER;
-    if (ritual_2->getName() != "unknown")  p2Ritual = ritual_2->display();
+    if (!ritual_2) p2Ritual = CARD_TEMPLATE_BORDER;
+    else if (ritual_2->getName() != "unknown")  p2Ritual = ritual_2->display();
     else p2Ritual = CARD_TEMPLATE_BORDER;
 
     vector<card_template_t> minions1;
